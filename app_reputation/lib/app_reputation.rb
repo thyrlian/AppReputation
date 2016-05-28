85390e7d08a244d17434b1514aead052d20b2ffb
Jing Li <thyrlian@gmail.com>
2016-05-29 01:23:04 +0200

    Create ruby gem skeleton

---
+++ app_reputation/lib/app_reputation.rb
@@ -0,0 +1,5 @@
+require "app_reputation/version"
+
+module AppReputation
+  # Your code goes here...
+end
---
+++ app_reputation/test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'app_reputation'
+
+require 'minitest/autorun'
