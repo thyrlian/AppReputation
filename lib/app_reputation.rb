b0a4bed041579a9d6b45e97bf615fc2930114091
Jing Li <thyrlian@gmail.com>
2016-06-14 22:28:46 +0200

    Require files recursively

--- lib/app_reputation.rb
+++ lib/app_reputation.rb
@@ -1,4 +1,4 @@
-Dir[(File.expand_path(File.dirname(__FILE__)) + "/app_reputation/*.rb")].each { |file| require file }
+Dir.glob(File.expand_path(File.dirname(__FILE__)) + '/app_reputation/**/*.rb').each { |file| require file }
 
 module AppReputation
   # Your code goes here...
