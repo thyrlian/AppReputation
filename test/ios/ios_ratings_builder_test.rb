c5c1248cf1ff1b60c5e4f17e1c0f6b7030e78d05
Jing Li <thyrlian@gmail.com>
2016-06-18 17:03:45 +0200

    Refactor IosRatingsBuilderTest

--- test/ios/ios_ratings_builder_test.rb
+++ test/ios/ios_ratings_builder_test.rb
@@ -1,26 +1,26 @@
 require 'test_helper'
 
 class IosRatingsBuilderTest < Minitest::Test
+  def setup
+    @header = AppReputation::IosRatingsBuilder.class_variable_get(:@@header_user_agent)
+    @resource = MiniTest::Mock.new
+    @ios_ratings_builder = AppReputation::IosRatingsBuilder.new
+  end
+  
   def test_get_ratings_valid
-    header = AppReputation::IosRatingsBuilder.class_variable_get(:@@header_user_agent)
     response = '{"ratingCountList":[1,2,3,5,8], "ratingCount": 19}'
-    resource = MiniTest::Mock.new
-    resource.expect(:get, response, [header])
-    ios_ratings_builder = AppReputation::IosRatingsBuilder.new
-    RestClient::Resource.stub(:new, resource) do
-      ratings = ios_ratings_builder.send(:get_ratings, 123456789, 'us')
+    @resource.expect(:get, response, [@header])
+    RestClient::Resource.stub(:new, @resource) do
+      ratings = @ios_ratings_builder.send(:get_ratings, 123456789, 'us')
       assert_equal(AppReputation::Ratings.new(1,2,3,5,8), ratings)
     end
   end
   
   def test_get_ratings_invalid
-    header = AppReputation::IosRatingsBuilder.class_variable_get(:@@header_user_agent)
     response = '{"iHaveNoRatingCountList": 0}'
-    resource = MiniTest::Mock.new
-    resource.expect(:get, response, [header])
-    ios_ratings_builder = AppReputation::IosRatingsBuilder.new
-    RestClient::Resource.stub(:new, resource) do
-      ratings = ios_ratings_builder.send(:get_ratings, 123456789, 'us')
+    @resource.expect(:get, response, [@header])
+    RestClient::Resource.stub(:new, @resource) do
+      ratings = @ios_ratings_builder.send(:get_ratings, 123456789, 'us')
       assert_nil(ratings)
     end
   end
