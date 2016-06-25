6aca96a95cb3d272423d4a2fe421e2646eaead27
Jing Li <thyrlian@gmail.com>
2016-06-25 09:32:43 +0200

    Add tests for get_ratings with timeout and retry

--- test/ios/ios_ratings_builder_test.rb
+++ test/ios/ios_ratings_builder_test.rb
@@ -12,6 +12,18 @@ class IosRatingsBuilderTest < Minitest::Test
     @ios_ratings_builder = AppReputation::IosRatingsBuilder.new
   end
   
+  def test_set_timeout
+    timeout = 4
+    AppReputation::IosRatingsBuilder.set_timeout(timeout)
+    assert_equal(timeout, AppReputation::IosRatingsBuilder.class_variable_get(:@@timeout))
+  end
+  
+  def test_set_max_retries
+    max_retries = 2
+    AppReputation::IosRatingsBuilder.set_max_retries(max_retries)
+    assert_equal(max_retries, AppReputation::IosRatingsBuilder.class_variable_get(:@@max_retries))
+  end
+  
   def test_get_ratings_valid
     response = '{"ratingCountList":[1,2,3,5,8], "ratingCount": 19}'
     @resource.expect(:get, response, [@header])
@@ -27,6 +39,27 @@ class IosRatingsBuilderTest < Minitest::Test
     RestClient::Resource.stub(:new, @resource) do
       ratings = @ios_ratings_builder.send(:get_ratings, 123456789, 'us')
       assert_nil(ratings)
+    end
+  end
+  
+  def test_get_ratings_succeed_after_retry
+    response = '{"ratingCountList":[1,2,3,5,8], "ratingCount": 19}'
+    resource = mock()
+    resource.stubs(:get).with(@header).raises(RestClient::RequestTimeout).then.returns(response)
+    RestClient::Resource.stub(:new, resource) do
+      ratings = @ios_ratings_builder.send(:get_ratings, 123456789, 'us')
+      assert_equal(AppReputation::Ratings.new(1,2,3,5,8), ratings)
+    end
+  end
+  
+  def test_get_ratings_fail_all_retries
+    def @resource.get(header)
+      raise RestClient::RequestTimeout
+    end
+    RestClient::Resource.stub(:new, @resource) do
+      assert_raises RestClient::RequestTimeout do
+        @ios_ratings_builder.send(:get_ratings, 123456789, 'us')
+      end
     end
   end
   
