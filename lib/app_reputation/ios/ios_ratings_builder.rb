abc7023bb9a1a62c110f2313166ffdb0434685fd
Jing Li <thyrlian@gmail.com>
2016-06-25 09:01:44 +0200

    Set timeout and add retry to IosRatingsBuilder

--- lib/app_reputation/ios/ios_ratings_builder.rb
+++ lib/app_reputation/ios/ios_ratings_builder.rb
@@ -7,7 +7,19 @@ module AppReputation
   class IosRatingsBuilder
     @@base_url = 'https://itunes.apple.com/$[country]/customer-reviews/id$[id]?dataOnly=true&displayable-kind=11&appVersion=all'
     @@header_user_agent = {'User-Agent' => 'iTunes/12.4.1 (Macintosh; OS X 10.11.5) AppleWebKit/601.6.17'}
+    @@timeout = 3
+    @@max_retries = 3
     @@supported_countries = Countries.parse(File.join(File.dirname(__FILE__), 'ios_country_code_list.txt'))
+    
+    class << self
+      def set_timeout(timeout)
+        @@timeout = timeout
+      end
+      
+      def set_max_retries(max_retries)
+        @@max_retries = max_retries
+      end
+    end
     
     def build(id, *countries)
       unless @@supported_countries.include?(*countries)
@@ -26,9 +38,19 @@ module AppReputation
     
     def get_ratings(id, country)
       url = @@base_url.set_param(:country, country).set_param(:id, id)
-      resource = RestClient::Resource.new(url, :verify_ssl => false)
-      response = resource.get(@@header_user_agent)
-      statistics = JSON.parse(response)['ratingCountList']
+      resource = RestClient::Resource.new(url, :verify_ssl => false, :open_timeout => @@timeout)
+      now_retry = 0
+      begin
+        response = resource.get(@@header_user_agent)
+        statistics = JSON.parse(response)['ratingCountList']
+      rescue RestClient::RequestTimeout => e
+        if now_retry < @@max_retries
+          now_retry += 1
+          retry
+        else
+          raise e
+        end
+      end
       if statistics
         return Ratings.new(*statistics)
       else
