55ef426c78cd59760bc0cdceb353ef501ff9b985
Jing Li <thyrlian@gmail.com>
2016-09-09 13:48:51 +0800

    Fix all tests in RestClientHelperTest

--- test/helper/rest_client_helper_test.rb
+++ test/helper/rest_client_helper_test.rb
@@ -1,44 +1,44 @@
 require_relative '../test_helper'
 
 class RestClientHelperTest < Minitest::Test
-  def test_send_request_get_with_block
-    headers = {'X-Requested-With' => 'XMLHttpRequest'}
+  def test_send_request_with_payload_and_block
+    method = mock()
     url = mock()
+    headers = mock()
+    payload = mock()
     response = mock()
-    response_history = mock()
-    response_history.stubs(:cookies).returns({'myacinfo' => 'f0F0f0F0'})
-    response.stubs(:history).returns([response_history])
-    response.stubs(:cookies).returns({'woinst' => '3072', 'wosid' => 'a1B2c3D4e5F6g7'})
-    expected_cookies = {'myacinfo' => 'f0F0f0F0', 'woinst' => '3072', 'wosid' => 'a1B2c3D4e5F6g7'}
-    expected_headers = headers.merge({:cookies => expected_cookies})
-    RestClient::Request.stub(:execute, response) do
-      AppReputation::RestClientHelper.send_request(:get, url, headers) do |res|
-        assert_equal(response, res)
-      end
-      assert_equal(expected_headers, headers)
+    args = {:method => method, :url => url, :verify_ssl => false, :headers => headers, :payload => payload}
+    
+    RestClient::Request.expects(:execute).with(args).returns(response).once
+    AppReputation::RestClientHelper.stubs(:handle_response).with(response, headers).returns(response).once
+    
+    actual_response = AppReputation::RestClientHelper.send_request(method, url, headers, payload) do |res|
+      assert_equal(response, res)
     end
+    assert_equal(response, actual_response)
   end
   
-  def test_send_request_post_without_block
-    headers = {'X-Requested-With' => 'XMLHttpRequest'}
-    payload = {'what' => 'ever'}
+  def test_send_request_without_payload_or_block
+    method = mock()
     url = mock()
+    headers = mock()
     response = mock()
-    response.stubs(:history).returns([])
-    response.stubs(:cookies).returns({'woinst' => '3072', 'wosid' => 'a1B2c3D4e5F6g7'})
-    expected_cookies = {'woinst' => '3072', 'wosid' => 'a1B2c3D4e5F6g7'}
-    expected_headers = headers.merge({:cookies => expected_cookies})
-    RestClient::Request.stub(:execute, response) do
-      AppReputation::RestClientHelper.send_request(:post, url, headers, payload)
-      assert_equal(expected_headers, headers)
-    end
+    args = {:method => method, :url => url, :verify_ssl => false, :headers => headers}
+    
+    RestClient::Request.expects(:execute).with(args).returns(response).once
+    AppReputation::RestClientHelper.stubs(:handle_response).with(response, headers).returns(response).once
+    
+    actual_response = AppReputation::RestClientHelper.send_request(method, url, headers)
+    assert_equal(response, actual_response)
   end
   
   def test_send_request_with_unauthorized_exception
     url = mock()
     headers = mock()
     payload = mock()
+    
     RestClient::Request.stubs(:execute).with(any_parameters).raises(RestClient::Unauthorized)
+    
     assert_raises AppReputation::Exception::UnauthorizedError do
       AppReputation::RestClientHelper.send_request(:post, url, headers, payload)
     end
@@ -49,9 +49,30 @@ class RestClientHelperTest < Minitest::Test
     headers = mock()
     payload = mock()
     response = mock()
+    
     RestClient::Request.stubs(:execute).with(any_parameters).raises(RestClient::Found)
     RestClient::Found.any_instance.stubs(:response).returns(response)
     AppReputation::RestClientHelper.expects(:handle_response).with(response, headers).returns(response).once
+    
     assert_equal(response, AppReputation::RestClientHelper.send_request(:post, url, headers, payload))
+  end
+  
+  def test_handle_response
+    headers = {'X-Info' => '001024'}
+    response = mock()
+    response_history = mock()
+    response_history.stubs(:cookies).returns({'code' => 'f0F0f0F0'})
+    response.stubs(:cookies).returns({'secret' => '1a2b3cXYZ', 'key' => 'a1B2c3D4e5F6g7'})
+    response.stubs(:history).returns([response_history])
+    
+    expected_cookies = {'secret' => '1a2b3cXYZ', 'key' => 'a1B2c3D4e5F6g7', 'code' => 'f0F0f0F0'}
+    expected_headers = headers.merge({:cookies => expected_cookies})
+    
+    actual_response = AppReputation::RestClientHelper.send(:handle_response, response, headers) do |res|
+      assert_equal(response, res)
+    end
+    
+    assert_equal(response, actual_response)
+    assert_equal(expected_headers, headers)
   end
 end
