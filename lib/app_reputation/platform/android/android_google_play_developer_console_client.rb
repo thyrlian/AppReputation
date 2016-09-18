7e9e2ab839b2a32bbc9d524017bab902e0443804
Jing Li <thyrlian@gmail.com>
2016-09-18 20:13:17 +0800

    Handle redirect url from response

--- lib/app_reputation/platform/android/android_google_play_developer_console_client.rb
+++ lib/app_reputation/platform/android/android_google_play_developer_console_client.rb
@@ -1,14 +1,12 @@
 require 'cgi'
 require 'webrick'
+require 'nokogiri'
 require_relative '../../helper/rest_client_helper'
 
 module AppReputation
   class AndroidGooglePlayDeveloperConsoleClient
-    @@continue_url = 'https://play.google.com/apps/publish/#'
-    @@followup_url = 'https://play.google.com/apps/publish/'
     @@main_url = 'https://play.google.com/apps/publish/'
-    @@login_url = "https://accounts.google.com/ServiceLogin?service=androiddeveloper&passive=1209600&continue=#{WEBrick::HTTPUtils.escape(@@continue_url)}&followup=#{@@followup_url}"
-    @@login_url_2 = 'https://accounts.google.com/accountLoginInfoXhr'
+    @@login_url = 'https://accounts.google.com/accountLoginInfoXhr'
     @@signin_url = 'https://accounts.google.com/signin/challenge/sl/password'
     
     attr_reader :headers
@@ -20,32 +18,36 @@ module AppReputation
     end
     
     def authenticate
+      header_connection = 'keep-alive'
       header_content_type = 'application/x-www-form-urlencoded'
       header_accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
       header_accept_encoding = 'gzip, deflate, br'
       header_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
-      headers = {'Content-Type' => header_content_type, 'Accept' => header_accept, 'Accept-Encoding' => header_accept_encoding, 'User-Agent' => header_user_agent}
+      headers = {'Connection' => header_connection, 'Content-Type' => header_content_type, 'Accept' => header_accept, 'Accept-Encoding' => header_accept_encoding, 'User-Agent' => header_user_agent}
       
       payload = {}
       
-      RestClientHelper.send_request(:get, @@main_url, headers)
+      new_url = ''
       
-      RestClientHelper.send_request(:get, @@login_url, headers) do |response|
+      RestClientHelper.send_request(:get, @@main_url, headers) do |response|
+        new_url = (Nokogiri::HTML(response.body).css('noscript').first.children.first.attribute('content').value.match(/url='(.*?)'/) || '')[1]
+      end
+      
+      RestClientHelper.send_request(:get, new_url, headers) do |response|
         payload = compose_payload(response.body) do |pl|
           pl.merge!({
             'Email' => @username,
-            'requestlocation' => @@login_url + '#identifier'
+            'requestlocation' => new_url + '#identifier'
           })
         end
       end
       
-      RestClientHelper.send_request(:post, @@login_url_2, headers.merge({'Referer' => @@login_url}), payload) do |response|
+      RestClientHelper.send_request(:post, @@login_url, headers.merge({'Referer' => new_url}), payload) do |response|
         payload['ProfileInformation'] = JSON.parse(response.body)['encoded_profile_information']
       end
       
       payload.delete('requestlocation')
       payload['Passwd'] = @password
-      new_url = ''
       
       RestClientHelper.send_request(:post, @@signin_url, headers, payload) do |response|
         if response.code == 302
