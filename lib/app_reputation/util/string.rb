3d5ba5a0836640edef9123e28b247178a501dc49
Jing Li <thyrlian@gmail.com>
2016-09-28 18:55:51 +0800

    Add escape_uri method

--- lib/app_reputation/util/string.rb
+++ lib/app_reputation/util/string.rb
@@ -2,4 +2,8 @@ class String
   def set_param(placeholder, value)
     self.gsub("$[#{placeholder}]", value.to_s)
   end
+  
+  def escape_uri
+    `echo #{self.inspect}`.chomp
+  end
 end
--- test/util/string_test.rb
+++ test/util/string_test.rb
@@ -11,4 +11,15 @@ class StringTest < Minitest::Test
     url = 'https://www.example.com/about'
     assert_equal(url, url.set_param(:name, 'acme'))
   end
+  
+  def test_escape_uri_positive
+    string = "https:\\x2F\\x2Faccounts.google.com\\x2Faccounts"
+    expected = 'https://accounts.google.com/accounts'
+    assert_equal(expected, string.escape_uri)
+  end
+  
+  def test_escape_uri_negative
+    string = 'This is a test!!!'
+    assert_equal(string, string.escape_uri)
+  end
 end
